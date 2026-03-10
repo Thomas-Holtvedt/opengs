121ed7b4ef27fe5bb5c9b82d99c00bbe62287f84
GermanCodeEngineer <GermanCodeEngineer@proton.me>
2026-03-10 21:56:36 +0100

    Performance upgrade

--- core/database/database.gd
+++ core/database/database.gd
@@ -3,6 +3,7 @@ class_name Database
 
 
 var tag_to_country: Dictionary[String, Country] = {}
+var lookup_to_color_province: Dictionary[Color, Color] = {} # Stores efficient to full color table
 var color_to_province: Dictionary[Color, Province] = {}
 var id_to_province: Dictionary[String, Province] = {}
 var id_to_territory: Dictionary[String, Territory] = {}
